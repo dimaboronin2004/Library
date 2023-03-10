2bcfc0b220488b225884a33e7dc84c49c9f5024d
dimaboronin2004 <boronin.dd@edu.spbstu.ru>
2023-03-11 02:06:35 +0300

    Added method toBook()

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -7,6 +7,8 @@ public final class Book {
     private String code;
 
     Book(String title, String author, String genre, String code) {
+        if (!code.matches("[А-Я]\\d+"))
+            throw new IllegalArgumentException("Wrong format of code");
         this.title = title;
         this.author = author;
         this.genre = genre;
@@ -30,11 +32,24 @@ public final class Book {
     }
 
     public void setCode(String code) {
+        if (!code.matches("[А-Я]\\d+"))
+            throw new IllegalArgumentException("Wrong format of code");
         this.code = code;
+    }
+
+    public Book toBook(String s) {
+        if (!s.matches("[А-я ]+, [А-я ]+, [А-я ]+, [А-я]\\d+"))
+            throw new IllegalArgumentException("String inconvertible to Book");
+        String title = s.split(", ")[0];
+        String author = s.split(", ")[1];
+        String genre = s.split(", ")[2];
+        String code = s.split(", ")[3];
+        return new Book(title, author, genre, code);
     }
 
     @Override
     public String toString() {
-        return title + " " + author + " " + genre + " " + code;
+        return title + ", " + author + ", " + genre + ", " + code;
     }
+
 }
